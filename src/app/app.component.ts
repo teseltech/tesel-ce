7ecaec597f2ebdcf7772a9ccfc9f083e002d0081
Ivan Reyes <ivanreyesconde@gmail.com>
2019-10-10 13:42:46 -0500

    archivos de ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class AppComponent {
-  title = 'tesel-ce';
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from "@angular/elements";
 
 import { AppComponent } from './app.component';
 
@@ -11,6 +12,13 @@ import { AppComponent } from './app.component';
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent] //  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    let custom = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define("custom-example", custom);
+  }
+}
